2e8887626af63547dcb7130e22566e3838fdaee1
Cristoper Aguilar <cjaguilar1997@gmail.com>
2024-07-08 11:58:59 -0600

    Version inicial de API de gestion de inventario y facturacion de tienda.

---
+++ src/test/java/com/tienda/cincomenos/CincomenosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tienda.cincomenos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CincomenosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
